c8bd2a3ee06d6942488d3a5125a951d9f0a72ba2
NcPulsa <46824241+Ro0TN3T@users.noreply.github.com>
2024-09-06 14:50:38 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -239,29 +239,7 @@ buffer = await imageToWebp(buff)}
 await NcPulsa.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
 return buffer}
  //=================================================//
-// NcPulsa.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
-// let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
-// let buffer
-// if (options && (options.packname || options.author)) {
-// buffer = await writeExifVid(buff, options)
-// } else {
-// buffer = await videoToWebp(buff)}
-// await NcPulsa.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
-// return buffer}
- //=================================================//
-//  NcPulsa.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
-// let quoted = message.msg ? message.msg : message
-// let mime = (message.msg || message).mimetype || ''
-// let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
-// const stream = await downloadContentFromMessage(quoted, messageType)
-// let buffer = Buffer.from([])
-// for await(const chunk of stream) {
-// buffer = Buffer.concat([buffer, chunk])}
-// let type = await FileType.fromBuffer(buffer)
-// trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
-// // save to file
-// await fs.writeFileSync(trueFileName, buffer)
-// return trueFileName}
+
 //=================================================
  NcPulsa.cMod = (jid, copy, text = '', sender = NcPulsa.user.id, options = {}) => {
 //let copy = message.toJSON()
@@ -285,24 +263,7 @@ copy.key.remoteJid = jid
 copy.key.fromMe = sender === NcPulsa.user.id
 return proto.WebMessageInfo.fromObject(copy)}
 
-// NcPulsa.sendFile = async(jid, PATH, fileName, quoted = {}, options = {}) => {
-// let types = await NcPulsa.getFile(PATH, true)
-// let { filename, size, ext, mime, data } = types
-// let type = '', mimetype = mime, pathFile = filename
-// if (options.asDocument) type = 'document'
-// if (options.asSticker || /webp/.test(mime)) {
-// let { writeExif } = require('./lib/sticker.js')
-// let media = { mimetype: mime, data }
-// pathFile = await writeExif(media, { packname: global.packname, author: global.packname2, categories: options.categories ? options.categories : [] })
-// await fs.promises.unlink(filename)
-// type = 'sticker'
-// mimetype = 'image/webp'}
-// else if (/image/.test(mime)) type = 'image'
-// else if (/video/.test(mime)) type = 'video'
-// else if (/audio/.test(mime)) type = 'audio'
-// else type = 'document'
-// await NcPulsa.sendMessage(jid, { [type]: { url: pathFile }, mimetype, fileName, ...options }, { quoted, ...options })
-// return fs.promises.unlink(pathFile)}
+
 
 NcPulsa.parseMention = async(text) => {
 return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}
@@ -334,24 +295,7 @@ contextInfo: {
 await NcPulsa.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
 return waMessage}
 //=================================================//
-// NcPulsa.getFile = async (PATH, save) => {
-// let res
-// let data = Buffer.isBuffer(PATH) ? PATH : /^data:.*?\/.*?;base64,/i.test(PATH) ? Buffer.from(PATH.split`,`[1], 'base64') : /^https?:\/\//.test(PATH) ? await (res = await getBuffer(PATH)) : fs.existsSync(PATH) ? (filename = PATH, fs.readFileSync(PATH)) : typeof PATH === 'string' ? PATH : Buffer.alloc(0)
-// //if (!Buffer.isBuffer(data)) throw new TypeError('Result is not a buffer')
-// let type = await FileType.fromBuffer(data) || {
-// mime: 'application/octet-stream',
-// ext: '.bin'
-// }
-// filename = path.join(__filename, '../src/' + new Date * 1 + '.' + type.ext)
-// if (data && save) fs.promises.writeFile(filename, data)
-// return {
-// res,
-// filename,
-// 	size: await getSizeMedia(data),
-// ...type,
-// data
-// }
-// }
+
 
 NcPulsa.serializeM = (m) => smsg(NcPulsa, m, store)
 NcPulsa.ev.on("connection.update", async (update) => {
